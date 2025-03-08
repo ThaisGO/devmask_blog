36c81797675cb48f82ffb23a613ef91722ed26a4
ThaisGO <thaisgo.viana@gmail.com>
2025-03-08 14:43:32 -0300

    feat: add boxicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css', 'boxicons/css/boxicons.min.css'],
   modules: ['@nuxtjs/tailwindcss', '@nuxt/content']
 })
