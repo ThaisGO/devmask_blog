37eaea9ed4e103e24651743e6c672cbb981b4c88
ThaisGO <thaisgo.viana@gmail.com>
2025-03-08 14:59:45 -0300

    added shacn ui package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,16 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css', 'boxicons/css/boxicons.min.css'],
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/content']
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/content', 'shadcn-nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
 })
