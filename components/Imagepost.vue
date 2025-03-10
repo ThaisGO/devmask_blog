ad8afcfd0ec1e4689b09c64fc2c3ea56b0b3d3da
ThaisGO <thaisgo.viana@gmail.com>
2025-03-09 21:45:38 -0300

    feat: new post components

---
+++ components/Imagepost.vue
@@ -0,0 +1,11 @@
+<script setup>
+    defineProps(['image', 'alt'])
+</script>
+
+<template>
+    <div v-if="image" class="max-w-[440px] h-[340px] relative  mx-auto my-8">
+        <img class="w-full h-full absolute object-cover"
+            :src="image"
+            :alt="alt">
+    </div>
+</template>
---
+++ components/Quote.vue
@@ -0,0 +1,12 @@
+<script setup>
+    defineProps(['icon'])
+</script>
+
+<template>
+    <div class="flex items-center gap-4 bg-zinc-900 text-lime-400 text-base mb-3 p-4 rounded-md">
+        <div>
+            <i :class="[icon]"></i>
+        </div> 
+        <slot mdc-unwrap="p" />
+    </div>
+</template>
